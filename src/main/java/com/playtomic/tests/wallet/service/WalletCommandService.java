e575b2a4a65b85add217ad81dd054b2ebd64ab39
Avelino <martinavelino91@gmail.com>
2021-08-02 00:49:55 +0200

    feat: pass WalletCommandService test for recharge when ok

--- src/main/java/com/playtomic/tests/wallet/service/WalletCommandService.java
+++ src/main/java/com/playtomic/tests/wallet/service/WalletCommandService.java
@@ -3,6 +3,7 @@ package com.playtomic.tests.wallet.service;
 import com.playtomic.tests.wallet.model.WalletDto;
 import com.playtomic.tests.wallet.persistance.WalletEntity;
 import com.playtomic.tests.wallet.persistance.WalletRepository;
+import com.playtomic.tests.wallet.service.impl.PayPalPaymentService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class WalletCommandService {
 
     private final ModelMapper modelMapper = new ModelMapper();
     private final WalletRepository walletRepository;
+    private final PayPalPaymentService payPalPaymentService;
 
     public Mono<WalletDto> charge(int id, String chargeAmount) {
         return walletRepository.findById(id)
@@ -30,6 +32,15 @@ public class WalletCommandService {
     }
 
     public Mono<WalletDto> recharge(int id, String rechargeAmount) {
-        return Mono.empty();
+        payPalPaymentService.charge(new BigDecimal(rechargeAmount));
+        return walletRepository.findById(id)
+                .map(walletEntity -> WalletEntity.builder()
+                        .id(walletEntity.getId())
+                        .amountCurrency(walletEntity.getAmountCurrency())
+                        .amountValue(walletEntity.getAmountValue().add(new BigDecimal(rechargeAmount)))
+                        .build())
+                .map(walletRepository::save)
+                .map(walletEntity -> Mono.just(modelMapper.map(walletEntity, WalletDto.class)))
+                .orElse(Mono.empty());
     }
 }
