3d312111a1eaf2a3808cf5517a7f139c3fe727f6
Avelino <martinavelino91@gmail.com>
2021-08-01 23:38:05 +0200

    test: pass WalletController test for find by id when ko

--- src/main/java/com/playtomic/tests/wallet/api/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/api/WalletController.java
@@ -1,15 +1,12 @@
 package com.playtomic.tests.wallet.api;
 
-import com.playtomic.tests.wallet.exception.WalletException;
 import com.playtomic.tests.wallet.model.WalletDto;
 import com.playtomic.tests.wallet.service.WalletQueryService;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -20,7 +17,9 @@ public class WalletController {
     private final WalletQueryService walletQueryService;
 
     @GetMapping("/wallet/{id}")
-    public Mono<ResponseEntity<WalletDto>> retrieveWalletById(@PathVariable int id) throws WalletException {
-        return Mono.empty();
+    public Mono<ResponseEntity<WalletDto>> retrieveWalletById(@PathVariable int id) {
+        return walletQueryService.retrieveWalletDataById(id)
+                .map(wallet -> new ResponseEntity<>(wallet, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
