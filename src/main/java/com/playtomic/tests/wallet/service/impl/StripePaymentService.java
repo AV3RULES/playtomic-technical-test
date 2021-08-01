5841a07b8d476d6699a7d9238aabec580502d3d4
Avelino <martinavelino91@gmail.com>
2021-08-02 01:36:20 +0200

    feat: pass test for new Third party payment service StripePaymentService for isSatisfiedBy and charge

--- src/main/java/com/playtomic/tests/wallet/service/impl/StripePaymentService.java
+++ src/main/java/com/playtomic/tests/wallet/service/impl/StripePaymentService.java
@@ -1,17 +1,24 @@
 package com.playtomic.tests.wallet.service.impl;
 
+import com.playtomic.tests.wallet.exception.WalletException;
 import com.playtomic.tests.wallet.service.ThirdPartyPaymentService;
+import org.springframework.http.HttpStatus;
 
 import java.math.BigDecimal;
+
+import static com.playtomic.tests.wallet.model.PaymentServiceType.STRIPE;
 
 public class StripePaymentService implements ThirdPartyPaymentService {
 
     @Override
     public boolean isSatisfiedBy(String paymentsType) {
-        return false;
+        return STRIPE.getName().equals(paymentsType);
     }
 
     @Override
     public void charge(BigDecimal amount) {
+        if (amount.doubleValue() != amount.intValue()) {
+            throw new WalletException(HttpStatus.BAD_REQUEST, "Stripe service not allow decimal charges");
+        }
     }
 }
