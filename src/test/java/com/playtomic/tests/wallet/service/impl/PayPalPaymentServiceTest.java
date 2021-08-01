9ed319d6f58bd35e7665efa5519bc3e4a244d608
Avelino <martinavelino91@gmail.com>
2021-08-02 00:42:24 +0200

    refactor: thirdparty payment test

--- src/test/java/com/playtomic/tests/wallet/service/impl/PayPalPaymentServiceTest.java
+++ src/test/java/com/playtomic/tests/wallet/service/impl/PayPalPaymentServiceTest.java
@@ -2,21 +2,26 @@ package com.playtomic.tests.wallet.service.impl;
 
 
 import com.playtomic.tests.wallet.exception.WalletException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PayPalPaymentServiceTest {
 
     PayPalPaymentService payPalPaymentService = new PayPalPaymentService();
 
-    @Test(expected = WalletException.class)
-    public void test_exception() throws WalletException {
-        payPalPaymentService.charge(new BigDecimal(5));
+    @Test
+    public void shouldThrowException_givenAmount_whenValueLowerThan10() throws WalletException {
+        WalletException expectedException = new WalletException(HttpStatus.BAD_REQUEST, "Paypal service not allow charges less than 10");
+        WalletException actualException = assertThrows(WalletException.class, () -> payPalPaymentService.charge(new BigDecimal(5)));
+        assertEquals(expectedException, actualException);
     }
 
     @Test
-    public void test_ok() throws WalletException {
-        payPalPaymentService.charge(new BigDecimal(15));
+    public void shouldNotThrowException_givenAmount_whenValueGreaterOrEqualsThan10() throws WalletException {
+        assertDoesNotThrow(() -> payPalPaymentService.charge(new BigDecimal(15)));
     }
 }
