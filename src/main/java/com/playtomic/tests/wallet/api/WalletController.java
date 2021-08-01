1dceca4e664bd137a10fe74334adce87994362d1
Avelino <martinavelino91@gmail.com>
2021-08-01 22:52:11 +0200

    test: create WalletController test for find by id when ok

--- src/main/java/com/playtomic/tests/wallet/api/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/api/WalletController.java
@@ -1,16 +1,26 @@
 package com.playtomic.tests.wallet.api;
 
+import com.playtomic.tests.wallet.exception.WalletException;
+import com.playtomic.tests.wallet.model.WalletDto;
+import com.playtomic.tests.wallet.service.WalletQueryService;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
+@AllArgsConstructor
 public class WalletController {
-    private Logger log = LoggerFactory.getLogger(WalletController.class);
 
-    @RequestMapping("/")
-    void log() {
-        log.info("Logging from /");
+    private final WalletQueryService walletQueryService;
+
+    @GetMapping("/wallet/{id}")
+    public Mono<ResponseEntity<WalletDto>> retrieveWalletById(@PathVariable int id) throws WalletException {
+        return Mono.empty();
     }
 }
---
+++ src/test/java/com/playtomic/tests/wallet/api/WalletControllerTest.java
@@ -0,0 +1,42 @@
+package com.playtomic.tests.wallet.api;
+
+import com.playtomic.tests.wallet.model.WalletDto;
+import com.playtomic.tests.wallet.persistance.WalletEntity;
+import com.playtomic.tests.wallet.persistance.WalletRepository;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class WalletControllerTest {
+
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    @Autowired
+    private WalletRepository walletRepository;
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void shouldReturnMonoResponseEntityOK_givenId_whenFindByIdOK() {
+        int givenId = 123;
+        WalletEntity givenWalletEntity = WalletEntity.builder().id(givenId).amountCurrency("EUR").amountValue(new BigDecimal("42.00")).build();
+        walletRepository.save(givenWalletEntity);
+
+        ResponseEntity<WalletDto> expectedResponseEntity = ResponseEntity.ok(modelMapper.map(givenWalletEntity, WalletDto.class));
+
+        webTestClient.get()
+                .uri("/wallet/{id}", givenId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(ResponseEntity.class)
+                .isEqualTo(expectedResponseEntity);
+    }
+}
