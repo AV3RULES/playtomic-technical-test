8916370a2d5c82a1dd6d888c4d4a2fb249c168d4
Avelino <martinavelino91@gmail.com>
2021-08-02 00:02:22 +0200

    feat: pass WalletCommandService test for charge when ok

--- src/main/java/com/playtomic/tests/wallet/service/WalletCommandService.java
+++ src/main/java/com/playtomic/tests/wallet/service/WalletCommandService.java
@@ -1,18 +1,31 @@
 package com.playtomic.tests.wallet.service;
 
 import com.playtomic.tests.wallet.model.WalletDto;
+import com.playtomic.tests.wallet.persistance.WalletEntity;
 import com.playtomic.tests.wallet.persistance.WalletRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class WalletCommandService {
 
+    private final ModelMapper modelMapper = new ModelMapper();
     private final WalletRepository walletRepository;
 
     public Mono<WalletDto> charge(int id, String chargeAmount) {
-        return Mono.empty();
+        return walletRepository.findById(id)
+                .map(walletEntity -> WalletEntity.builder()
+                        .id(walletEntity.getId())
+                        .amountCurrency(walletEntity.getAmountCurrency())
+                        .amountValue(walletEntity.getAmountValue().subtract(new BigDecimal(chargeAmount)))
+                        .build())
+                .map(walletRepository::save)
+                .map(walletEntity -> Mono.just(modelMapper.map(walletEntity, WalletDto.class)))
+                .get();
     }
 }
