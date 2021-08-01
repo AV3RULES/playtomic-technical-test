9b5e1c7c98db83349e60959dcff8b6d450cae2df
Avelino <martinavelino91@gmail.com>
2021-08-01 22:14:38 +0200

    feat: pass WalletQueryService test for find by id when ko

--- src/main/java/com/playtomic/tests/wallet/service/WalletQueryService.java
+++ src/main/java/com/playtomic/tests/wallet/service/WalletQueryService.java
@@ -1,10 +1,11 @@
 package com.playtomic.tests.wallet.service;
 
+import com.playtomic.tests.wallet.exception.WalletException;
 import com.playtomic.tests.wallet.model.WalletDto;
 import com.playtomic.tests.wallet.persistance.WalletRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -16,7 +17,8 @@ public class WalletQueryService {
     private final ModelMapper modelMapper = new ModelMapper();
 
     public Mono<WalletDto> retrieveWalletDataById(int id) {
-        return Mono.just(walletRepository.findById(id)
-                .map(walletEntity -> modelMapper.map(walletEntity, WalletDto.class)).get());
+        return walletRepository.findById(id)
+                .map(walletEntity -> Mono.just(modelMapper.map(walletEntity, WalletDto.class)))
+                .orElse(Mono.error(new WalletException(HttpStatus.NOT_FOUND, "Wallet with id " + id + " not found")));
     }
 }
